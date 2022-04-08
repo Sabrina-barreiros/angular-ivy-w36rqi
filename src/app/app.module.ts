fe012f3468c9ce4a005a5939aa876d482a1a6f06
Sabrina-barreiros <barreiros890@gmail.com>
2022-04-08 15:08:10 -0300

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NumbersComponent } from './numbers/numbers.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+    {path:'', component: HomeComponent}, 
+    {path:'about', component: AboutComponent},
+    {path:'numbers', component: NumbersComponent}
+  ])
+ ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
